e751b049fcf2684fb95f3dd169d875c39ce60ab0
Harsh Mriduhash <Harshsahay2709@gmail.com>
2025-01-13 14:04:01 +0530

    Fixing

--- src/services/Location.service.ts
+++ src/services/Location.service.ts
@@ -1,52 +1,52 @@
-import { NextRouter } from 'next/router';
+import { NextRouter } from "next/router";
 
 interface QueryParams {
-	[key: string]: string | string[] | undefined;
+  [key: string]: string | string[] | undefined;
 }
 
 class LocationService {
-	private router: NextRouter;
+  private router: NextRouter;
 
-	constructor(router: NextRouter) {
-		this.router = router;
-	}
+  constructor(router: NextRouter) {
+    this.router = router;
+  }
 
-	public updateQueryParam(param: string, value: string): void {
-		const currentPathname = this.router.pathname;
-		const currentQuery = this.router.query;
+  public updateQueryParam(param: string, value: string): void {
+    const currentPathname = this.router.pathname;
+    const currentQuery = this.router.query;
 
-		const newQuery = {
-			...currentQuery,
-			[param]: value,
-		};
+    const newQuery = {
+      ...currentQuery,
+      [param]: value,
+    };
 
-		this.navigateTo({
-			pathname: currentPathname,
-			query: newQuery,
-		});
-	}
+    this.navigateTo({
+      pathname: currentPathname,
+      query: newQuery,
+    });
+  }
 
-	public removeQueryParam(param: string): void {
-		const currentPathname = this.router.pathname;
-		const currentQuery = { ...this.router.query };
+  public removeQueryParam(param: string): void {
+    const currentPathname = this.router.pathname;
+    const currentQuery = { ...this.router.query };
 
-		if (currentQuery[param]) {
-			delete currentQuery[param];
-			this.navigateTo({ pathname: currentPathname, query: currentQuery });
-		}
-	}
+    if (currentQuery[param]) {
+      delete currentQuery[param];
+      this.navigateTo({ pathname: currentPathname, query: currentQuery });
+    }
+  }
 
-	public getQueryParams(): QueryParams {
-		return this.router.query;
-	}
+  public getQueryParams(): QueryParams {
+    return this.router.query;
+  }
 
-	public getPathname(): string {
-		return this.router.pathname;
-	}
+  public getPathname(): string {
+    return this.router.pathname;
+  }
 
-	private navigateTo(route: { pathname: string; query: QueryParams }): void {
-		this.router.push(route, undefined, { shallow: true });
-	}
+  private navigateTo(route: { pathname: string; query: QueryParams }): void {
+    this.router.push(route, undefined, { shallow: true });
+  }
 }
 
 export default LocationService;
