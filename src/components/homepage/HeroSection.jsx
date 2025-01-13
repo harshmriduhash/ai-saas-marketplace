546928387d1497203d955cf38542d5295b56d4e1
Harsh Mriduhash <Harshsahay2709@gmail.com>
2025-01-13 13:56:57 +0530

    Fixing

--- src/components/homepage/HeroSection.jsx
+++ src/components/homepage/HeroSection.jsx
@@ -1,15 +1,14 @@
-import React from 'react'
+import React from "react";
 
 function HeroSection() {
   return (
-    <div className='hero-section'>
-
-        <h1>
-            <span>Expert developers for</span> <br />
-            <span>a connected</span> <span>{"{World}"}</span>
-        </h1>
+    <div className="hero-section">
+      <h1>
+        <span>Expert developers for</span> <br />
+        <span>a connected</span> <span>{"{World}"}</span>
+      </h1>
     </div>
-  )
+  );
 }
 
-export default HeroSection
+export default HeroSection;
